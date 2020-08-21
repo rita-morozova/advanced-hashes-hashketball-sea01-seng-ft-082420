39a531db64693303b645e66c7fc9fe55cac96c38
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-21 20:52:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,6 +151,14 @@ def team_colors(team_name)
   game_hash.each do |team, team_info|
     if team_info[:team_name] == team_name
     return team_info[:colors]
+    end 
   end 
 end 
+
+def team_names
+  array= []
+  game_hash.each do |team, team_info|
+    array << team_info[:team_name]
+  end 
+  return array 
 end 
